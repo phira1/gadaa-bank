6d980defdb5397a3ddcf60cbfaa669c8625c4b0c
Firaol Bekuma <fraolbek11@gmail.com>
2026-01-28 11:19:26 +0300

    NUCLEAR: change output folder to break CDN cache

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
+    outDir: 'build',  // CHANGE from 'dist' to 'build'
     rollupOptions: {
       output: {
-        // FORCE new file names every build
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]'
