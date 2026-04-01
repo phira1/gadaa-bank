62ac1f03b597b65d665a17e9f6311919f353eac2
Firaol Bekuma <fraolbek11@gmail.com>
2026-04-01 16:18:36 +0300

    Improve homepage responsiveness: update LookingFor, Services, About, Difference, Contact components

--- src/data/regulatoryData.js
+++ src/data/regulatoryData.js
@@ -1,7 +1,28 @@
 // src/data/regulatoryData.js
+
 export const nbeDocuments = [
-  { title: "Directive No. SBB/91/2024 - Corporate Governance", date: "January 2024", fileSize: "1.2 MB", link: "#" },
-  { title: "Directive No. FIS/01/2023 - Interest Rate Framework", date: "March 2023", fileSize: "0.8 MB", link: "#" },
-  { title: "Guideline on Loan Classification and Provisioning", date: "December 2023", fileSize: "2.1 MB", link: "#" },
-  { title: "Anti-Money Laundering and Combating Financing of Terrorism Directive", date: "August 2023", fileSize: "3.4 MB", link: "#" }
+  {
+    title: "Directive No. SBB/91/2024 - Corporate Governance",
+    date: "January 2024",
+    fileSize: "1.2 MB",
+    link: "#"
+  },
+  {
+    title: "Directive No. FIS/01/2023 - Interest Rate Framework",
+    date: "March 2023",
+    fileSize: "0.8 MB",
+    link: "#"
+  },
+  {
+    title: "Guideline on Loan Classification and Provisioning",
+    date: "December 2023",
+    fileSize: "2.1 MB",
+    link: "#"
+  },
+  {
+    title: "Anti-Money Laundering and Combating Financing of Terrorism Directive",
+    date: "August 2023",
+    fileSize: "3.4 MB",
+    link: "#"
+  }
 ];
