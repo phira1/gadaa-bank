a8f6c43b5bff6b5227d80bbd78d520ae30a090ca
Firaol Bekuma <fraolbek11@gmail.com>
2026-04-01 10:45:34 +0300

    Update departments: replace sensitive phone numbers with placeholders (xxx)

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -2,17 +2,17 @@ import { FaWhatsapp, FaTelegram, FaFacebook } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 
 export const contactInfo = [
-  { icon: 'FaPhone', title: 'Call us', details: '+251 641', description: 'Main Office Line' },
+  { icon: 'FaPhone', title: 'Call us', details: '+251 116392578', description: 'Main Office Line' },
   { icon: 'FaHeadset', title: 'Hotline', details: '641', description: '24/7 Customer Service' },
   { icon: 'FaEnvelope', title: 'Email', details: 'info@gadaabank.com.et', description: 'General Inquiries' },
-  { icon: 'FaMapMarkerAlt', title: 'Head Office', details: 'Gotera, kirkos SubCity W-03, HNo-#745', description: 'Addis Ababa, Ethiopia' }
+  { icon: 'FaMapMarkerAlt', title: 'Head Office', details: 'Gotera, Kirkos SubCity W-03, HNo-#745', description: 'Addis Ababa, Ethiopia' }
 ];
 
 export const departments = [
   { name: 'Customer Service', email: 'support@gadaabank.com.et', phone: '641' },
-  { name: 'Account Opening', email: 'accounts@gadaabank.com.et', phone: '+251 116392500' },
-  { name: 'Loan Department', email: 'loans@gadaabank.com.et', phone: '+251 116392501' },
-  { name: 'Card Services', email: 'cards@gadaabank.com.et', phone: '+251 116392502' }
+  { name: 'Account Opening', email: 'accounts@gadaabank.com.et', phone: 'xxx' },
+  { name: 'Loan Department', email: 'loans@gadaabank.com.et', phone: 'xxx' },
+  { name: 'Card Services', email: 'cards@gadaabank.com.et', phone: 'xxx' }
 ];
 
 export const socialMedia = [
@@ -23,8 +23,8 @@ export const socialMedia = [
 ];
 
 export const workingHours = [
-  { day: 'Monday - Thursday', time: '8:30 AM - 5:30 PM' },
-  { day: 'Friday', time: '8:30 AM - 11:30 AM, 1:30 PM - 5:30 PM' },
-  { day: 'Saturday', time: '8:30 AM - 5:30 PM' },
+  { day: 'Monday - Thursday', time: '8:00 AM - 5:30 PM' },
+  { day: 'Friday', time: '8:00 AM - 11:30 AM, 1:30 PM - 5:30 PM' },
+  { day: 'Saturday', time: '8:00 AM - 5:30 PM' },
   { day: 'Sunday', time: 'Closed' }
 ];
